d9e6b1e735197b9054e9b026a412abc87488d927
Yaroslavom <yarykomm@gmail.com>
2021-07-05 02:17:46 +0300

    Switch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 import Header from "../Header/Header";
 import RandomPlanet from "../Random-planet/";
 import ErrorIndicator from "../Error-indicator/Error-indicator";
@@ -7,14 +11,20 @@ import SwapiService from "../../Services/Swapi-service";
 import DummySwapiService from "../../Services/Dummy-swapi-service";
 import { SwapiServiceProvider } from "../Swapi-service-context/Swapi-service-context";
 import "./App.css";
-import { PeoplePage, PlanetPage, StarshipPage, MeetPage, LoginPage } from "../Pages";
+import {
+  PeoplePage,
+  PlanetPage,
+  StarshipPage,
+  MeetPage,
+  LoginPage,
+} from "../Pages";
 import { StarshipDetails } from "../Sw-components";
 
 export default class App extends Component {
   state = {
     hasError: false,
     swapiService: new SwapiService(),
-    isLoggedIn: false
+    isLoggedIn: false,
   };
 
   componentDidCatch() {
@@ -33,39 +43,56 @@ export default class App extends Component {
       };
     });
 
-    onLogin=()=> {
-      this.setState({isLoggedIn: true})
-    }
+  onLogin = () => {
+    this.setState({ isLoggedIn: true });
+  };
 
   render() {
     if (this.state.hasError) {
       return <ErrorIndicator />;
     }
 
-    const {isLoggedIn} = this.state
+    const { isLoggedIn } = this.state;
 
     return (
-      <div className="stardb-app">
-        <SwapiServiceProvider value={this.state.swapiService}>
+      <SwapiServiceProvider value={this.state.swapiService}>
+        <div className="stardb-app">
           <Router>
             <Header onServiceChange={this.onServiceChange} />
             <RandomPlanet />
-            <Route path="/" render={() => <h2 className="text-center">Welcome to Star DB</h2>} exact />
-            <Route path="/people/:id?" component={PeoplePage} />
-            <Route path="/planets" component={PlanetPage} /> 
-            {/* ↑ Пример как работает код без переключения реакт роутера (данный код не хранит в себе выбранный id при обновлении страницы )*/}
-            <Route path="/starships" exact component={StarshipPage} />
-            <Route
-              path="/starships/:id"
-              render={({ match }) => (
-                <StarshipDetails itemId={match.params.id} />
-              )}
-            />
-            <Route path="/meet" render={()=><MeetPage isLoggedIn={isLoggedIn}/>}/>
-            <Route path="/login" component={()=><LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin}/>}/>
+            <Switch>
+              <Route
+                path="/"
+                render={() => (
+                  <h2 className="text-center">Welcome to Star DB</h2>
+                )}
+                exact
+              />
+              <Route path="/people/:id?" component={PeoplePage} />
+              <Route path="/planets" component={PlanetPage} />
+              {/* ↑ Пример как работает код без переключения реакт роутера (данный код не хранит в себе выбранный id при обновлении страницы )*/}
+              <Route path="/starships" exact component={StarshipPage} />
+              <Route
+                path="/starships/:id"
+                render={({ match }) => (
+                  <StarshipDetails itemId={match.params.id} />
+                )}
+              />
+              <Route
+                path="/meet"
+                render={() => <MeetPage isLoggedIn={isLoggedIn} />}
+              />
+              <Route
+                path="/login"
+                component={() => (
+                  <LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin} />
+                )}
+              />
+              <Route render={()=><h2>Page not found</h2>}/>
+            </Switch>
           </Router>
-        </SwapiServiceProvider>
-      </div>
+        </div>
+      </SwapiServiceProvider>
     );
   }
 }
